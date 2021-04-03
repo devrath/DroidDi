7d4e83cb96fd8e83721e5c8d5900457ecaa6dd2d
Devrath AD <devrathad@mplgaming.com>
2021-04-03 20:31:12 +0530

    v

--- app/src/main/java/com/demo/code/MainActivity.kt
+++ app/src/main/java/com/demo/code/MainActivity.kt
@@ -13,14 +13,19 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.demo.code.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val toolbar: Toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
 
@@ -29,15 +34,13 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
         }
-        val drawerLayout: DrawerLayout = findViewById(R.id.drawer_layout)
-        val navView: NavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_koin, R.id.nav_dagger, R.id.nav_hilt), drawerLayout)
+                R.id.nav_koin, R.id.nav_dagger, R.id.nav_hilt), binding.drawerLayout)
         setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+        binding.navView.setupWithNavController(navController)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/demo/code/ui/dagger/DaggerSelectionFragment.kt
+++ app/src/main/java/com/demo/code/ui/dagger/DaggerSelectionFragment.kt
@@ -9,8 +9,12 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.demo.code.R
+import com.demo.code.databinding.FragmentDaggerSelectionBinding
 
 class DaggerSelectionFragment : Fragment() {
+
+    private var _binding: FragmentDaggerSelectionBinding? = null
+    private val binding get() = _binding!!
 
     private lateinit var daggerSelectionViewModel: DaggerSelectionViewModel
 
@@ -19,13 +23,8 @@ class DaggerSelectionFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        daggerSelectionViewModel =
-                ViewModelProvider(this).get(DaggerSelectionViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_dagger_selection, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        daggerSelectionViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        daggerSelectionViewModel =ViewModelProvider(this).get(DaggerSelectionViewModel::class.java)
+        _binding = FragmentDaggerSelectionBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
