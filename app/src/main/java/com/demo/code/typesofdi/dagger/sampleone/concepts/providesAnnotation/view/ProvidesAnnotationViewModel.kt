1a488311b58caca2730618e131fd49b2601bc9e9
Devrath AD <devrathad@mplgaming.com>
2021-04-11 20:06:45 +0530

    v

--- app/src/main/java/com/demo/code/typesofdi/dagger/sampleone/concepts/providesAnnotation/view/ProvidesAnnotationViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.demo.code.typesofdi.dagger.sampleone.concepts.providesAnnotation.view
-
-import androidx.lifecycle.ViewModel
-
-class ProvidesAnnotationViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-}
