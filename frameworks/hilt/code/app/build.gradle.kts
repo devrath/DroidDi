0afc6c93d9326e5bd899954b15e612764b247ca4
devrath dev <devrath.dev595@gmail.com>
2023-09-17 09:23:12 +0530

    added end points

--- frameworks/hilt/code/app/build.gradle.kts
+++ frameworks/hilt/code/app/build.gradle.kts
@@ -20,6 +20,10 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        
+        // End point URL
+        val url: String = "https://5e510330f2c0d300147c034c.mockapi.io/"
+        buildConfigField("String", "BASE_URL", "\"$url\"")
     }
 
     buildTypes {
@@ -50,7 +54,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -58,7 +62,27 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
+    implementation("androidx.activity:activity-ktx:1.7.2")
+
     // <-- Hilt ->
     implementation("com.google.dagger:hilt-android:2.44")
     kapt("com.google.dagger:hilt-android-compiler:2.44")
+
+    // <-- Recycler view ->
+    implementation("androidx.recyclerview:recyclerview:1.3.1")
+    implementation("android.arch.lifecycle:extensions:1.1.1")
+
+    // <-- Image Library ->
+    implementation("com.github.bumptech.glide:glide:4.11.0")
+
+
+    // <-- Networking ->
+    implementation("com.squareup.retrofit2:converter-moshi:2.6.2")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.2")
+
+    // <-- Coroutine ->
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
 }
