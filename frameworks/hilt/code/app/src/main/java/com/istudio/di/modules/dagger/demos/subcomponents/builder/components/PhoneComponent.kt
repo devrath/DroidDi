187e6db157969317367f0e343e3b53fa4be9507c
devrath dev <devrath.dev595@gmail.com>
2023-09-27 17:48:55 +0530

    check

--- frameworks/hilt/code/app/src/main/java/com/istudio/di/modules/dagger/demos/subcomponents/builder/components/PhoneComponent.kt
+++ frameworks/hilt/code/app/src/main/java/com/istudio/di/modules/dagger/demos/subcomponents/builder/components/PhoneComponent.kt
@@ -11,7 +11,7 @@ interface PhoneComponent {
     fun inject(activity: DaggerSubComponentsBuilderActivity)
 
 
-    @Subcomponent.Factory
+    @Subcomponent.Builder
     interface Builder{
         fun build() : PhoneComponent
 
