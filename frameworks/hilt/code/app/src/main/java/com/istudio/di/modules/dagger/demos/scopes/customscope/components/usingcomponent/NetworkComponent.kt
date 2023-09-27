768a73d7301fac71da31ef8429b2ac28d740d56a
devrath dev <devrath.dev595@gmail.com>
2023-09-27 12:10:24 +0530

    Initial changes

--- frameworks/hilt/code/app/src/main/java/com/istudio/di/modules/dagger/demos/scopes/customscope/components/usingcomponent/NetworkComponent.kt
+++ frameworks/hilt/code/app/src/main/java/com/istudio/di/modules/dagger/demos/scopes/customscope/components/usingcomponent/NetworkComponent.kt
@@ -1,15 +1,11 @@
 package com.istudio.di.modules.dagger.demos.scopes.customscope.components.usingcomponent
 
-import com.istudio.di.modules.dagger.demos.scopes.applicationscope.scope.ApplicationScope
 import com.istudio.di.modules.dagger.demos.scopes.customscope.CustomScopeActivity
 import com.istudio.di.modules.dagger.demos.scopes.customscope.components.usingmodule.ApplicationComponent
-import com.istudio.di.modules.dagger.demos.scopes.customscope.implementations.usingcomponent.logger.CustomLogger
-import com.istudio.di.modules.dagger.demos.scopes.customscope.implementations.usingcomponent.logger.TimberLogger
 import com.istudio.di.modules.dagger.demos.scopes.customscope.modules.usingcomponet.CustomLoggerModule
 import com.istudio.di.modules.dagger.demos.scopes.customscope.modules.usingcomponet.CustomRetrofitModule
 import com.istudio.di.modules.dagger.demos.scopes.customscope.modules.usingcomponet.TimberLoggerModule
 import com.istudio.di.modules.dagger.demos.scopes.customscope.scopes.ActivityScope
-import dagger.BindsInstance
 import dagger.Component
 
 
