801c7dea382420ffc59cc96df04c8a340c9aaeb1
devrath dev <devrath.dev595@gmail.com>
2023-09-28 17:28:20 +0530

    New selection

---
+++ frameworks/hilt/code/app/src/main/java/com/istudio/di/modules/dagger/demos/multibindings/elementsintoset/DaggerMultiBindingElementsIntoSetActivity.kt
@@ -0,0 +1,12 @@
+package com.istudio.di.modules.dagger.demos.multibindings.elementsintoset
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.istudio.di.R
+
+class DaggerMultiBindingElementsIntoSetActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dagger_multi_binding_elements_into_set)
+    }
+}
