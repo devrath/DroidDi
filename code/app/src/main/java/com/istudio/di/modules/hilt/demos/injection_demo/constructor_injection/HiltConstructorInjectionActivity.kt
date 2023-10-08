d742aec85f2a00e11375a4937b8f6050b3fa74de
devrath dev <devrath.dev595@gmail.com>
2023-10-08 21:52:00 +0530

    set edited

--- code/app/src/main/java/com/istudio/di/modules/hilt/demos/injection_demo/constructor_injection/HiltConstructorInjectionActivity.kt
+++ code/app/src/main/java/com/istudio/di/modules/hilt/demos/injection_demo/constructor_injection/HiltConstructorInjectionActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.istudio.di.databinding.ActivityHiltConstructorInjectionBinding
 import com.istudio.di.modules.hilt.demos.injection_demo.constructor_injection.implementation.Car
+import com.istudio.di.utils.PrintUtils
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -24,6 +25,7 @@ class HiltConstructorInjectionActivity : AppCompatActivity() {
     private fun setOnClickListeners() {
         binding.apply {
             initiateId.setOnClickListener {
+                PrintUtils.printLog("Car instance -> "+car.hashCode().toString())
                 car.start()
             }
         }
