46cc20e93c990c9bb0cabff46c52654f29d69204
devrath dev <devrath.dev595@gmail.com>
2023-10-09 21:12:44 +0530

    new changes

--- code/app/src/main/java/com/istudio/di/modules/hilt/demos/scoping_demo/implementations/HiltNetworkService.kt
+++ code/app/src/main/java/com/istudio/di/modules/hilt/demos/scoping_demo/implementations/HiltNetworkService.kt
@@ -3,7 +3,13 @@ package com.istudio.di.modules.hilt.demos.scoping_demo.implementations
 import com.istudio.di.utils.PrintUtils
 
 class HiltNetworkService {
+
+    init {
+        PrintUtils.printLog("HiltNetworkService class is built")
+    }
+
+
     fun callNetworkEndPoint(data : String) {
-        PrintUtils.printLog(data)
+        PrintUtils.printLog("Reference address:-> $data")
     }
 }
